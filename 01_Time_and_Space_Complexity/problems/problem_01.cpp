886de5c8a392962a4eee2dcb270b9936158d4b36
Sagar Sharma <sagarsharma131006@gmail.com>
2026-08-28 18:12:50 +0530

    Add Solution class for Two Sum problem
    Initial implementation of the Two Sum problem class.

---
+++ 01_Time_and_Space_Complexity/problems/problem_01.cpp
@@ -0,0 +1,13 @@
+// LeetCode #1 - Two Sum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        // Your solution here
+        
+    }
+};
