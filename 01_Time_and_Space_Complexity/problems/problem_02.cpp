5b51ece54175e3f8c95433dde8213df10a46c104
Sagar Sharma <sagarsharma131006@gmail.com>
2026-08-28 18:13:51 +0530

    Add initial implementation for containsDuplicate method

---
+++ 01_Time_and_Space_Complexity/problems/problem_02.cpp
@@ -0,0 +1,13 @@
+// LeetCode #217 - Contains Duplicate
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        // Your solution here
+        
+    }
+};
