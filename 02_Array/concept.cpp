75352882988658ecbb89c63da3d913d87f7ddf3f
Sagar Sharma <sagarsharma131006@gmail.com>
2026-09-06 16:04:36 +0530

    Add conceptual overview for array topics
    This file serves as a conceptual overview of arrays, outlining various topics related to arrays such as traversal, insertion, deletion, searching, and algorithms.

---
+++ 02_Array/concept.cpp
@@ -0,0 +1,15 @@
+// Arrays - Concepts
+// Topic: Arrays
+//
+// This file contains:
+// - Array basics
+// - Traversal
+// - Insertion
+// - Deletion
+// - Searching
+// - Two Pointer
+// - Prefix Sum
+// - Sliding Window
+// - Kadane's Algorithm
+//
+// Concepts will be added as they are learned.
