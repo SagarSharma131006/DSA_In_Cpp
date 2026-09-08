4e92ff23ad76f25071efc997a88c9b2487dcdd73
Sagar Sharma <sagarsharma131006@gmail.com>
2026-09-08 09:35:17 +0530

    Add shuffle function for array rearrangement
    Implement shuffle function to rearrange array elements.

---
+++ 02_Array/Problems/019_Shuffle_The_Array.cpp
@@ -0,0 +1,21 @@
+// LeetCode #1470 - Shuffle the Array
+// Difficulty: Easy
+// Topic: Arrays
+// Approach: Direct Indexing + Extra Array
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(2 * n);
+
+        for (int i = 0; i < n; i++) {
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[i + n];
+        }
+
+        return ans;
+    }
+};
+/*
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
