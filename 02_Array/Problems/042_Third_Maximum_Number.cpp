aad196cdd347246cd9e88f1de4081caf6ede32ea
Sagar Sharma <sagarsharma131006@gmail.com>
2026-09-18 20:55:03 +0530

    Add solution for third maximum number problem
    Implement solution to find the third maximum number in an array.

---
+++ 02_Array/Problems/042_Third_Maximum_Number.cpp
@@ -0,0 +1,36 @@
+// LeetCode #414 - Third Maximum Number
+// Difficulty: Easy
+// Topic: Arrays
+// Approach: Three Maximums Tracking
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long first = LONG_MIN;
+        long second = LONG_MIN;
+        long third = LONG_MIN;
+
+        for (int num : nums) {
+            if (num == first || num == second || num == third)
+                continue;
+
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            }
+            else if (num > second) {
+                third = second;
+                second = num;
+            }
+            else if (num > third) {
+                third = num;
+            }
+        }
+
+        return (third == LONG_MIN) ? first : third;
+    }
+};
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
